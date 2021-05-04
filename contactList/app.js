da226af640aa92fedaa7812e338ac57b126d97ac
isrt09 <mazedur@gmail.com>
2021-05-04 14:53:57 +0600

    Contact Route is done

--- contactList/app.js
+++ contactList/app.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const app     = express()
-const port    = process.env.PORT || 3000
+const express      = require('express')
+const app          = express()
+const morgan       = require('morgan')
+const port         = process.env.PORT || 3000
+const contactRoute = require('./contactRoute')
 
+app.use(morgan('dev'))
+app.use('/contacts',contactRoute)
+
+app.get('*', (req,res)=>{
+    res.send(`<h1>404 - Not Found !!!</h1>`)
+})
 app.listen(port, ()=>{
     console.log(`Server is running at localhost:${port}`);
 })
